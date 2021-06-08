39256e8dcfa67118b89e7c50b4d35f023cfe380e
hsdxm <744263061@qq.com>
2021-06-08 14:52:39 +0800

    修改yolov5安装脚本

--- InstallYolov5TrainEnv.sh
+++ InstallYolov5TrainEnv.sh
@@ -1,10 +1,10 @@
 conda create --name  yolov5 python=3.7.9 -y
 source activate
 conda activate yolov5
-pip install -r requirements.txt
 
 git clone https://github.com/ultralytics/yolov5.git
 cd yolov5
+pip install -r requirements.txt
 git reset --hard 69be8e738
 
 python models/export.py --weights ../last.pt
